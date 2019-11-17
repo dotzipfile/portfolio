9a3d953d1d656c376becc6bee2b0d6d1f38cd9ec
David Malone <davenickmalone@gmail.com>
2019-11-17 20:54:57 +0000

    Tidy up

---
+++ src/components/Music/Music.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Music extends Component {
+    render() {
+        return (
+            <div>This is a place for music</div>
+        );
+    }
+}
+
+export default Music;

d3c94a9e107f843f57616ebe3e7f13747bf25905
David Malone <davenickmalone@gmail.com>
2019-11-21 22:16:25 +0000

    Add video list

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 import Card from '../../containers/Card/Card';
+import classes from './Music.css';
 
 class Music extends Component {
     render() {
         return (
             <Card>
-                This is my card
+                <ul className={classes.List}>
+                    <li>
+                        <h2>Wicked Game</h2>
+                        <iframe title="Wicked Game" width="560" height="315" src="https://www.youtube.com/embed/z2l67EtTKxQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <hr />
+                    </li>
+                    <li>
+                        <h2>High Hopes</h2>
+                        <iframe title="High Hopes" width="560" height="315" src="https://www.youtube.com/embed/PA0XK1DM51g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <hr />
+                    </li>
+                    <li>
+                        <h2>Amie</h2>
+                        <iframe title="Amie" width="560" height="315" src="https://www.youtube.com/embed/alHarpr9X7g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </li>
+                </ul>
             </Card>
         );
     }
